9731380c65b71fd15fe20c531b401652600652ae
유보배 <bbyu@Bobae.local>
2020-10-10 15:27:40 +0900

    express : router

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhosting:${PORT}`);
+};
+
+function handleHome(req, res) {
+    console.log(req);
+    res.send('Hello from home');
+}
+app.get('/', handleHome); 
+
+app.listen(PORT , handleListening);
